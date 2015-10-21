fde0df0775fecc61c30ee3f20d8ccc6caa07f0e1
Michael Yemets <mgyemets@gmail.com>
2015-10-21 18:41:10 +0300

    1.0.12

--- app/src/main/java/com/govmap/activity/MapActivity.java
+++ app/src/main/java/com/govmap/activity/MapActivity.java
@@ -1,14 +1,17 @@
 package com.govmap.activity;
 
+import android.app.Dialog;
 import android.content.ActivityNotFoundException;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
@@ -17,8 +20,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
@@ -152,6 +157,7 @@ public class MapActivity extends BaseActivity implements
 
     @Override
     protected void onDestroy() {
+        mGoogleApiClient.disconnect();
         mGovmapFrame.removeView(wvGov);
         ((MainApplication) getApplication()).clearHandlers();
         super.onDestroy();
@@ -222,9 +228,31 @@ public class MapActivity extends BaseActivity implements
     public void onConnectionSuspended(int i) { }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
-//        notFoundAddress();
-//        animateMapToLocation();
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mProgressDialog != null && mProgressDialog.isShowing())
+            mProgressDialog.dismiss();
+
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            Toast.makeText(MapActivity.this, "", Toast.LENGTH_LONG).show();
+            return;
+        }
+        else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+                if (mProgressDialog != null && !mProgressDialog.isShowing())
+                    mProgressDialog.show();
+            }
+        } else {
+            // Show dialog using GoogleApiAvailability.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
+        }
+
     }
 
     @Override
@@ -281,7 +309,8 @@ public class MapActivity extends BaseActivity implements
                 mSatellite.setTextColor(getResources().getColor(R.color.blue_dark));
 
                 mGovmapFrame.setVisibility(View.VISIBLE);
-
+                if (!TextUtils.isEmpty(mData.getSearchAddress()))
+                    wvGov.loadUrl(String.format("javascript:(function() {document.getElementById('tbSearchWord').value = '%s';})();", mData.getSearchAddress()));
             }
 
         }
@@ -316,9 +345,13 @@ public class MapActivity extends BaseActivity implements
                 mBlockSmooth.setText(getString(R.string.text_cadastre_not_found));
             }
             else {
-                String cadastralString = String.format(getString(R.string.text_lot_parcel),
-                        String.valueOf(mData.getLot()),
-                        String.valueOf(mData.getParcel()));
+                String cadastralString = getString(R.string.text_lot)
+                                        + " "
+                                        + String.valueOf(mData.getLot())
+                                        + " "
+                                        + getString(R.string.text_parcel)
+                                        + " "
+                                        + String.valueOf(mData.getParcel());
 
                 mBlockSmooth.setText(cadastralString);
             }
@@ -689,4 +722,57 @@ public class MapActivity extends BaseActivity implements
                 .create().show();
     }
 
+    //region handle connection failures
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    private static final String DIALOG_ERROR = "dialog_error";
+    private boolean mResolvingError = false;
+
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GoogleApiAvailability.getInstance().getErrorDialog(
+                    this.getActivity(), errorCode, REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MapActivity) getActivity()).onDialogDismissed();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+    //endregion
 }
+
